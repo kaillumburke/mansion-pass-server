1beaadb8ab1c3781e5790ba256416914da7db186
Eric McKenna <eric@ericmckenna.co.uk>
2026-06-12 11:41:29 +0100

    Initial Commit

---
+++ Mansion Nightclub/Mansion_NightclubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mansion_NightclubApp.swift
+//  Mansion Nightclub
+//
+//  Created by Eric McKenna on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Mansion_NightclubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
